fd4889f3aa163389108a1bfff2954dd6b41619b9
Adam Jensen <acjensen@gmail.com>
2019-10-06 14:29:46 -0400

    Remove .env support

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,21 +2,6 @@
 
 set -eu
 
-# Bail out (but report success) if there's no work to do
-if [ -f "$GITHUB_WORKSPACE/.env/NOTHING_TO_DO" ]; then
-  exit 0
-fi
-
-function export_to_env {
-  name=$(echo $1 | awk '{ print toupper($0)}')
-  value=$(cat "$GITHUB_WORKSPACE"/.env/$1)
-  export $name="$value"
-}
-
-for var in $(ls "$GITHUB_WORKSPACE"/.env); do
-  export_to_env $var
-done
-
 # Generate a temporary key for abuild
 mkdir "$GITHUB_WORKSPACE"/.abuild
 echo "$GITHUB_WORKSPACE/key" | abuild-keygen
