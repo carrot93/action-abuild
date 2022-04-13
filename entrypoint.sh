3201720fd4df8646aca457526434c611dd73b608
Adam Jensen <adam@acj.sh>
2022-04-13 07:09:52 -0400

    Remove extra git fetch

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,10 +11,6 @@ echo PACKAGER_PRIVKEY="$GITHUB_WORKSPACE/key" > "$HOME"/.abuild/abuild.conf
 
 cd "$GITHUB_WORKSPACE/$INPUT_PACKAGE_PATH"
 
-# Fetch latest from upstream since this fork may be out of date
-git remote add upstream https://github.com/alpinelinux/aports.git
-git fetch upstream && git reset --hard upstream/master
-
 # Update the package version
 sed -i "s/pkgver=.*/pkgver=$INPUT_RELEASE_VERSION/g" APKBUILD
 
